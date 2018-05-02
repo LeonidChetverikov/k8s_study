507f8ffce2fbd346efcad8b7d6545cea675b2dd9
LeonidChetverikov <34073185+LeonidChetverikov@users.noreply.github.com>
2018-05-02 15:10:26 +0300

    Create exporter-outside.go

---
+++ exporter-outside.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"os"
+	"path/filepath"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/kubernetes"
+	metricapi "github.com/kubernetes/dashboard/src/app/backend/integration/metric/api"
+	
+
+)
+
+
+var (
+	Pod = "nginx"
+	Namespace = "Web"
+	cpuRequest = prometheus.NewGauge(prometheus.GaugeOpts{
+		Subsystem: Pod,
+		Namespace: Namespace,
+		Name:      "pod_resource_requests_cpu",
+		Help:      "Cpu usage.",
+	})
+	memRequest = prometheus.NewGauge(
+		prometheus.GaugeOpts{
+			Subsystem: Pod,
+			Namespace: Namespace,
+			Name: "pod_resource_requests_mem",
+			Help: "Memory usage.",
+		})
+	)
+
+func init() {
+	// Metrics have to be registered to be exposed:
+	prometheus.MustRegister(cpuRequest)
+	prometheus.MustRegister(memRequest)
+}
+
+func main() {
+
+	var kubeconfig *string
+	if home := homeDir(); home != "" {
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	} else {
+		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	}
+	flag.Parse()
+
+	// use the current context in kubeconfig
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// create the clientset
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+	cpu, err := strconv.ParseFloat(metricapi.CpuUsage, 64)
+	mem, err := strconv.ParseFloat(metricapi.MemoryUsage, 64)
+	cpuRequest.Set(cpu)
+	memRequest.Set(mem)
+	// The Handler function provides a default handler to expose metrics
+	// via an HTTP server. "/metrics" is the usual endpoint for that.
+	http.Handle("/metrics", promhttp.Handler())
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+func homeDir() string {
+	if h := os.Getenv("HOME"); h != "" {
+		return h
+	}
+	return os.Getenv("USERPROFILE")
+}
+
